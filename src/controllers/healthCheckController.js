12fdaa8c372b985b8d68317b408fb9c67e33a44f
Nikitha Kambhampati <kambhampati.ni@northeastern.edu>
2024-09-26 10:18:15 -0400

    checks for json mafunction

--- src/controllers/healthCheckController.js
+++ src/controllers/healthCheckController.js
@@ -2,11 +2,18 @@ const sequelize = require('../config/dbConfig');
 const { setResponse, setErrorResponse } = require('../middlewares/errorHandlers'); 
 
 const healthCheck = async (req, res) => {
-    if (Object.keys(req.query).length > 0 || req.headers['content-length'] > 0 || (req.body && Object.keys(req.body).length > 0)) {
-        return setResponse(req, res); 
-    }
-
     try {
+        if (Object.keys(req.query).length > 0) {
+            return res.status(400).send(); 
+        }
+
+        if (req.body && (typeof req.body !== 'object' || Array.isArray(req.body))) {
+            return res.status(400).send(); 
+        }
+
+        if (req.headers['content-length'] > 0 && !req.body) {
+            return res.status(400).send(); 
+        }
         await sequelize.authenticate();
         setResponse(req, res); 
     } catch (error) {
