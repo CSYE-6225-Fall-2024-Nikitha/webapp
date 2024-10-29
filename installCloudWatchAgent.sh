9de67228cc8aaf5265cf37707525103938cd7d2a
Nikitha Kambhampati <kambhampati.ni@northeastern.edu>
2024-10-28 21:02:53 -0400

    script fix

--- installCloudWatchAgent.sh
+++ installCloudWatchAgent.sh
@@ -20,7 +20,7 @@ sudo chown -R csye6225:csye6225 /home/ubuntu
 sudo chown -R csye6225:csye6225 /home/ubuntu/webapp
 sudo chmod 755 /home/ubuntu/webapp
 sudo chmod -R 755 /home/ubuntu/webapp
-mkdir -p /home/ubuntu/webapp/logs
+sudo mkdir -p /home/ubuntu/webapp/logs
 
 # Enable and start the CloudWatch Agent service using the configuration
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
